35bd680e4dc18e90eafab3ba72a3a65273ea2a24
innoCyber <innocentileka@gmail.com>
2020-05-23 15:48:38 +0100

    created inner class _MappingPageState

--- lib/Mapping.dart
+++ lib/Mapping.dart
@@ -1,3 +1,23 @@
 import 'package:flutter/material.dart';
 import 'Authentication.dart';
 
+class MappingPage extends StatefulWidget{
+  final AuthImplementation auth;
+
+  MappingPage({
+    this.auth,
+  });
+
+  State<StatefulWidget> createState(){
+    return _MappingPageState();
+  }
+}
+
+
+
+class _MappingPageState extends State<MappingPage>{
+  @override
+  Widget build(BuildContext context) {
+    return null;
+  }
+}
